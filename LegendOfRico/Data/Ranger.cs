ac46bee4ad3a0080323927afbe7fbb78f6fb8a6e
Thomas <t.copin@yahoo.fr>
2023-11-18 12:22:55 +0100

    Ajout du ranger

--- LegendOfRico/Data/Ranger.cs
+++ LegendOfRico/Data/Ranger.cs
@@ -2,5 +2,6 @@ namespace LegendOfRico.Data;
 
 public class Ranger : Character
 {
-
+    public Weapon[] WeaponMastery { get; private set; }
+    public TypeOfArmor ArmorMastery { get; } = TypeOfArmor.Medium;
 }
