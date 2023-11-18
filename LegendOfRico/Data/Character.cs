7471af43986bde75f5d708b466c05b25c7084f9d
Asta Martin <martin.asta@hotmail.com>
2023-11-18 11:20:26 +0100

    Character devient classe abstraite plutôt qu'une interface + début de classe Ranger

--- LegendOfRico/Data/Character.cs
+++ LegendOfRico/Data/Character.cs
@@ -1,10 +1,15 @@
 namespace LegendOfRico.Data;
 
-public interface ICharacter
+public abstract class Character
 {
     public string Name { get; private set; }
     public int Level { get; private set; }
     public int HitPoints { get; private set; }
     public Dictionary<Stats, int> Statistics { get; private set; }
     public int Armor { get; private set; }
+
+    public CreateCharacter
+    {
+        //To be defined
+    }
 }
