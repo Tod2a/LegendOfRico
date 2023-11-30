5952e8bf78f3c0f8dc25233a16146b0e13494495
57406 <57406@etu.he2b.be>
2023-11-30 20:09:30 +0100

    Les sorts sont correctement mis par défaut

--- LegendOfRico/Data/Cleric.cs
+++ LegendOfRico/Data/Cleric.cs
@@ -6,14 +6,8 @@ public class Cleric : Character
     public override TypeOfArmor ArmorMastery => TypeOfArmor.Medium;
     public override bool CanEquipShield { get; protected set; } = true;
     public override int MaxHitPoints => 25;
-    public override List<Spells> SpellBook { get; protected set; } = new List<Spells>();
+    public override List<Spells> SpellBook { get; protected set; } = new List<Spells>() { new Smite(), new Heal(),};
     public override string fightImgUrl { get;  } = "img/Character/fightCleric.png";
-
-    public void InitializeSpellBook()
-    {
-        SpellBook.Add(new Smite());
-        SpellBook.Add(new Heal());
-    }
 
     public override void Rest()
     {
--- LegendOfRico/Data/Wizard.cs
+++ LegendOfRico/Data/Wizard.cs
@@ -4,15 +4,10 @@
     {
         public override TypeOfWeapon[] WeaponMastery => new[] { TypeOfWeapon.Staff };
         public override TypeOfArmor ArmorMastery => TypeOfArmor.Light;
-        public override List<Spells> SpellBook { get; protected set; } = new List<Spells>();
+        public override List<Spells> SpellBook { get; protected set; } = new List<Spells>() { new Fireball() };
         public override bool CanEquipShield { get; protected set; } = false;
         public override int MaxHitPoints => 20;
         public override string fightImgUrl { get; } = "img/Character/fightWizard.png";
-
-        public void InitializeSpellBook()
-        {
-            SpellBook.Add(new Fireball());
-        }
 
         public override void Rest()
         {
