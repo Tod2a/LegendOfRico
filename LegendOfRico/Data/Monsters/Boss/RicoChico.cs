3399132c4f5619b7aa090f168500b46bdafcfc12
tod2a <t.copin@yahoo.fr>
2023-12-05 18:53:20 +0100

    Ajout du boss Ricochico

---
+++ LegendOfRico/Data/Monsters/Boss/RicoChico.cs
@@ -0,0 +1,21 @@
+﻿namespace LegendOfRico.Data
+{
+    public class RicoChico: Boss
+    {
+        public override string MonsterName { get; set; } = "Le monstrueux RicoChico";
+        public override int MonsterCurrentHP { get; set; } = 10000;
+        public override int MonsterHP { get; set; } = 10000;
+        public override MonsterHit[] HitTable { get; set; } = new MonsterHit[] { };
+        public override TypeOfBreed MonsterBreed { get; set; } = TypeOfBreed.RicoChico;
+        public override int XpGranted { get; set; } = 50000;
+        public override string fightImgUrl { get; set; } = "img/monster/Boss/RicoChico.png";
+
+        protected override MonsterHit[] BuildHitTable()
+        {
+            return new MonsterHit[]
+            {
+                new MonsterHit{Name = "Dance des hanches", MinDamage = 200, MaxDamage = 500}
+            };
+        }
+    }
+}
