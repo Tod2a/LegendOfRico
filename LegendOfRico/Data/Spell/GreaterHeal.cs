7646233b09883065af115c38cbee6a0e0140ec2e
Asta Martin <martin.asta@hotmail.com>
2023-12-09 11:33:55 +0100

    Ajouté deux nouveaux sorts au clerc

---
+++ LegendOfRico/Data/Spell/GreaterHeal.cs
@@ -0,0 +1,47 @@
+﻿namespace LegendOfRico.Data;
+
+public class GreaterHeal : Spells
+{
+    public override string SpellName { get; protected set; } = "Soin supérieur (10/10)";
+    public override int MaxNumberOfUses => 10;
+    public override int CurrentNumberOfUses { get; protected set; } = 10;
+    public int MinValue => 15;
+    public int MaxValue => 20;
+    public double CritChance = 0.15;
+
+    public override string UseSpell(Character player, Monster target)
+    {
+        string s = "";
+
+        if (CurrentNumberOfUses > 0)
+        {
+            int healRoll = (new Random()).Next(MinValue, MaxValue + 1);
+            healRoll += (player.Statistics / 50) * healRoll;
+
+            if ((new Random()).NextDouble() <= CritChance)
+            {
+                s += "Coup critique !";
+                healRoll *= 2;
+            }
+            player.ReceiveHeal(healRoll);
+            if (player.PartyMember != null)
+            {
+                player.PartyMember.ReceiveHeal(healRoll);
+                s += player.Name + " soigne le groupe de " + healRoll + " points de vie!";
+            }
+            else
+            {
+                s += player.Name + " est soigné de " + healRoll + " points de vie!";
+            }
+
+            CurrentNumberOfUses--;
+            SpellName = "Soin supérieur (" + CurrentNumberOfUses + "/" + MaxNumberOfUses + ")";
+        }
+        else
+        {
+            s += "Vous ne pouvez plus lancer ce sort !";
+        }
+        player.SetIsRested(false);
+        return s;
+    }
+}
---
+++ LegendOfRico/Data/Spell/SupremeHeal.cs
@@ -0,0 +1,36 @@
+﻿namespace LegendOfRico.Data;
+
+public class SupremeHeal : Spells
+{
+    public override string SpellName { get; protected set; } = "Soin suprême (1/1)";
+    public override int MaxNumberOfUses => 1;
+    public override int CurrentNumberOfUses { get; protected set; } = 1;
+
+    public override string UseSpell(Character player, Monster target)
+    {
+        string s = "";
+
+        if (CurrentNumberOfUses > 0)
+        {
+            player.ReceiveHeal(player.MaxHitPoints);
+            if (player.PartyMember != null)
+            {
+                player.PartyMember.ReceiveHeal(player.PartyMember.MaxHitPoints);
+                s += player.Name + " rend tous ses points de vie au groupe ! ";
+            }
+            else
+            {
+                s += player.Name + " s'est rendu tous ses points de vie ! ";
+            }
+
+            CurrentNumberOfUses--;
+            SpellName = "Soin suprême (" + CurrentNumberOfUses + "/" + MaxNumberOfUses + ")";
+        }
+        else
+        {
+            s += "Vous ne pouvez plus lancer ce sort !";
+        }
+        player.SetIsRested(false);
+        return s;
+    }
+}
