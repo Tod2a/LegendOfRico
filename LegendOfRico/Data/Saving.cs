90f1ad845b2bd9642b7e8df1ef7f66f1d975ffcb
tod2a <t.copin@yahoo.fr>
2023-12-16 16:39:42 +0100

    Premiere partie de sauvegarde

---
+++ LegendOfRico/Data/Saving.cs
@@ -0,0 +1,40 @@
+﻿namespace LegendOfRico.Data
+{
+    public class Saving
+    {
+        public string CharactType { get; set; }
+
+        public Saving()
+        {
+        }
+
+        public Saving (Character player)
+        {
+            CharactType = GetStringType(player);
+        }
+
+        private string GetStringType(Character player)
+        {
+            if (player.GetType() == typeof(Rogue)) 
+            {
+                return "rogue";
+            }
+            else if (player.GetType() == typeof(Fighter))
+            {
+                return "fighter";
+            }
+            else if (player.GetType() == typeof(Ranger))
+            {
+                return "ranger";
+            }
+            else if (player.GetType() == typeof(Cleric))
+            {
+                return "cleric";
+            }
+            else
+            {
+                return "wizard";
+            }
+        }
+    }
+}
